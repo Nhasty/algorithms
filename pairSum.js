6f583d14f061dd5b643d18443f46e1aa5e19d72a
nhasty <nyall.hasty@gmail.com>
2022-12-04 12:02:35 -0800

    pairSum success

---
+++ pairSum.js
@@ -0,0 +1,21 @@
+// Write a function, pairSum, that takes in an array and a target sum as arguments. The function should return an array containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+const pairSum = (numbers, targetSum) => {
+  // todo
+  // iterate numbers
+  numbers.forEach((num1, i, nums) => {
+    nums.slice(i+1).forEach((num2, j) => {
+      if ((num1 + num2) === targetSum) {
+        return [i, (i +j + 1)]
+      }
+    })
+  })
+    // iterate every number after current number
+    // if both numbers sum to target
+      // return array of indices
+
+};
