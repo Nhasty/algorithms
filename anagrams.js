c7cc6a9133d23fd72fb5ae0391050eeba72289df
nhasty <nyall.hasty@gmail.com>
2022-12-04 11:46:49 -0800

    anagrams success

--- anagrams.js
+++ anagrams.js
@@ -1,7 +1,7 @@
-Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+// Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
 
-test_00:
-anagrams('restful', 'fluster'); // -> true
+// test_00:
+// anagrams('restful', 'fluster'); // -> true
 
 const anagrams = (s1, s2) => {
   // todo
@@ -14,12 +14,12 @@ const anagrams = (s1, s2) => {
     if (map1[s1[i]] === undefined) {
       map1[s1[i]] = 1
     } else {
-      map1[s1[1]]++
+      map1[s1[i]]++
     }
     if (map2[s2[i]] === undefined) {
       map2[s2[i]] = 1
     } else {
-      map2[s2[1]]++
+      map2[s2[i]]++
     }
   }
   for (key in map1) {
