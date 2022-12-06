2d139fc5f38cbb843e6765ba947e3bd5f5de454b
nhasty <nyall.hasty@gmail.com>
2022-12-05 21:49:06 -0800

    getNodeValue rec and iter optimal

---
+++ linkedLists/getNodeValue.js
@@ -0,0 +1,24 @@
+// Write a function, getNodeValue, that takes in the head of a linked list and an index. The function should return the value of the linked list at the specified index.
+
+// If there is no node at the given index, then return null.
+
+
+// iterative
+
+const getNodeValue = (head, index) => {
+  // todo
+  let currentIndex = 0;
+  let current = head
+  while (current !== null) {
+    if (currentIndex === index) return current.val
+    current = current.next;
+    currentIndex += 1;
+  }
+  return null;
+};
+
+// recursive
+
+const getNodeValue = (head, index) => {
+  // todo
+};
