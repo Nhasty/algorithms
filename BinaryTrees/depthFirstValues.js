a9d973009005e3a70dcc1d237bd70e129fac8a68
nhasty <nyall.hasty@gmail.com>
2022-12-08 09:42:21 -0800

    depthFirstValues rec and iter

--- BinaryTrees/depthFirstValues.js
+++ BinaryTrees/depthFirstValues.js
@@ -2,14 +2,46 @@
 
 // Hey. This is our first binary tree problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
 
-// class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
+// iterative
 
 const depthFirstValues = (root) => {
   // todo
+  // initialize stack
+  if (root === null) return [];
+  const stack = [ root ];
+  // storage
+  const values = [];
+  // while stack not empty
+  while (stack.length) {
+    const visiting = stack.pop();
+    values.push(visiting.val);
+    if (visiting.right !== null) {
+      stack.push(visiting.right);
+    }
+    if (visiting.left !== null) {
+      stack.push(visiting.left);
+    }
+  }
+  return values;
+    // pop stack
+    // add value to storage
+    //add right and left if they exist
+};
+
+// recursive
+
+const depthFirstValues = (root) => {
+  // todo
+  if (root === null) {
+    return [];
+  }
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+  return [root.val, ...leftValues, ...rightValues];
+  // add root val to array
+  // if root left exists
+    // push call self on root.left
+  // if root right exists
+    // push call self on root.right
+  // return array
 };
