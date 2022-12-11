43415e6f3b5991a2a6f4d39f3d5a4e3e7d3cb138
nhasty <nyall.hasty@gmail.com>
2022-12-11 08:50:13 -0800

    howHigh df rec

--- BinaryTrees/howHigh.js
+++ BinaryTrees/howHigh.js
@@ -3,3 +3,12 @@
 // The height of a binary tree is defined as the maximal number of edges from the root node to any leaf node.
 
 // If the tree is empty, return -1.
+
+// df recursive
+
+const howHigh = (node) => {
+  // todo
+  if (node === null) return -1;
+  let count = 1;
+  return (count + Math.max(howHigh(node.left), howHigh(node.right)));
+}
