9b5db8bf421af057c10a2d628f754ff29b247998
nhasty <nyall.hasty@gmail.com>
2022-12-16 17:50:23 -0800

    prereqsPossible start

---
+++ Graphs/prereqPossible.js
@@ -0,0 +1,7 @@
+// Write a function, prereqsPossible, that takes in a number of courses (n) and prerequisites as arguments. Courses have ids ranging from 0 through n - 1. A single prerequisite of [A, B] means that course A must be taken before course B. The function should return a boolean indicating whether or not it is possible to complete all courses.
+
+const prereqsPossible = (numCourses, prereqs) => {
+  // todo
+  const visited = new Set();
+  const graph = writeUnidirectionalGraph(numCourses, pre)
+};
