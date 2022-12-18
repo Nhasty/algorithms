b0c73727d6d70be292b643359728c1b85350eabc
nhasty <nyall.hasty@gmail.com>
2022-12-18 09:53:14 -0800

    prereqsPossible solution

--- Graphs/prereqPossible.js
+++ Graphs/prereqPossible.js
@@ -3,5 +3,40 @@
 const prereqsPossible = (numCourses, prereqs) => {
   // todo
   const visited = new Set();
-  const graph = writeUnidirectionalGraph(numCourses, pre)
+  const graph = writeUnidirectionalGraph(numCourses, prereqs);
+  for (let course in graph) {
+    if (detectCycle(graph, course, visited, new Set())) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const writeUnidirectionalGraph = (numCourses, prereqs) => {
+  const graph = {};
+  for (let i = 0; i < numCourses; i += 1) {
+    graph[i] = [];
+  };
+  for (let prereq of prereqs) {
+    const [a, b] = prereq;
+    graph[a].push(String(b));
+  };
+  return graph;
+};
+
+const detectCycle = (graph, node, visited, visiting) => {
+  if (visited.has(node)) {
+    return false;
+  }
+  if (visiting.has(node)) {
+    return true;
+  }
+  visiting.add(node);
+  for (let neighbor of graph[node]) {
+    if (detectCycle(graph, neighbor, visited, visiting)) {
+      return true;
+    }
+  }
+  visited.add(node);
+  return false;
 };
