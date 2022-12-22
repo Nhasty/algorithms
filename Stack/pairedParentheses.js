e87350020f3ee9a41d62fa9c25042cc1d329a318
nhasty <nyall.hasty@gmail.com>
2022-12-22 09:12:45 -0800

    pairedParenthes iterative O(n) time O(1) space

--- Stack/pairedParentheses.js
+++ Stack/pairedParentheses.js
@@ -1,4 +1,6 @@
+// Write a function, pairedParentheses, that takes in a string as an argument. The function should return a boolean indicating whether or not the string has well-formed parentheses.
 
+// You may assume the string contains only alphabetic characters, '(', or ')'.
 
 const pairedParentheses = (str) => {
   let count = 0;
