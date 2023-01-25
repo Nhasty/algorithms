b3ab8b0707719cb7061602227f970e11630514e9
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-01-25 13:19:32 -0700

    Time: 73 ms (80.30%), Space: 43.8 MB (90.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,5 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-  const setNums = new Set(nums);
-  for (let i = 0; i <= nums.length; i += 1) {
-    if (!(setNums.has(i))) {
-      return i
-    }
-  }
+  return nums.reduce((memo, num, i) => i + 1 - num + memo, 0)
 };
