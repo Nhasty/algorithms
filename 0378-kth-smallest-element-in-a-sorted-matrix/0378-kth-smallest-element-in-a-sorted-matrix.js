9934a052926e624875baf1beae5b56b58970a050
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-01-28 12:34:07 -0700

    Time: 165 ms (29.23%), Space: 62.1 MB (5.99%) - LeetHub

---
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+  let array = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    array = [...array, ...matrix[i]];
+  };
+    array.sort((a, b) => a - b);
+  return array[k - 1];
+};
