5632ca5fb4bfab903164074b47f1bc4e4ab23fcf
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-01-31 16:53:23 -0700

    Time: 61 ms (79.68%), Space: 41.5 MB (83.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,33 +3,32 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-  if (!matrix.length || !matrix[0].length) {
-    return []
-  }
-  if (matrix.length === 1) {
-    return matrix.pop();
-  }
-  return [...matrix.shift(), ...downRight(matrix), ...matrix.pop().reverse(), ...upLeft(matrix), ...spiralOrder(matrix)];
-};
-
-const downRight = (matrix) => {
-  if (!matrix.length || !matrix[0].length) {
-    return [];
-  }
-    const output = [];
-  for (let i = 0; i < matrix.length; i += 1) {
-    output.push(matrix[i].pop());
+  let output = [];
+  let top = 0;
+  let right = matrix[0].length - 1;
+  let bottom = matrix.length - 1;
+  let left = 0;
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i += 1) {
+      output.push(matrix[top][i])
+    }
+    top += 1;
+    for (let j = top; j <= bottom; j += 1) {
+      output.push(matrix[j][right]);
+    }
+    right -= 1;
+    if (top <= bottom) {
+      for (let k = right; k >= left; k -=1) {
+        output.push(matrix[bottom][k]);
+      }
+    }
+    bottom -= 1;
+    if (left <= right) {
+      for (let l = bottom; l >= top; l -= 1) {
+        output.push(matrix[l][left]);
+      }
+    }
+    left += 1;
   }
   return output;
-}
-
-const upLeft = (matrix) => {
-  if (!matrix.length || !matrix[0].length) {
-    return [];
-  }
-    const output = [];
-  for (let j = matrix.length - 1; j >= 0; j -= 1) {
-    output.push(matrix[j].shift())
-  }
-  return output
-}
+};
