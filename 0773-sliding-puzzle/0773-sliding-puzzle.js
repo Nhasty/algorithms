f894cd752e52d89731287cf52e761699d2a89380
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-02-02 13:43:32 -0700

    Time: 87 ms (76.47%), Space: 49.4 MB (32.35%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.js
+++ 0773-sliding-puzzle/0773-sliding-puzzle.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var slidingPuzzle = function(board) {
+debugger
   const distances = [
+                      [3, 2, 1, 3, 1, 0],
                       [0, 1, 2, 1, 2, 3],
                       [1, 0, 1, 2, 1, 2],
                       [2, 1, 0, 3, 2, 1],
                       [1, 2, 3, 0, 1, 2],
                       [2, 1, 2, 1, 0, 1],
-                      [3, 2, 1, 2, 1, 0],
                     ];
   const moves = {
     0: [1, 3],
@@ -27,22 +28,23 @@ var slidingPuzzle = function(board) {
     array[move] = 0;
     return array.join('');
   }
-  const iterations = new Set(state);
+  const iterations = new Set();
+  iterations.add(state);
   const q = [[state, state.indexOf('0'), 0]];
   while (q.length) {
     const [current, zero, moveCount] = q.shift();
-    iterations.add(current);
     if (current === '123450') {
       return moveCount;
     }
     for (let move of moves[zero]) {
       const next = movePieces(current, zero, move);
+      const score = next.split('').reduce((memo, number, index) => memo + distances[number][index], 0)
       if (!iterations.has(next)) {
-        q.push([next, move, moveCount + 1]);
+        q.push([next, move, moveCount + 1, moveCount + 1 + score]);
         iterations.add(next)
       }
     }
+    q.sort((a, b) => a[3] - b[3]);
   }
-  return -1
-   
+  return -1   
 };
