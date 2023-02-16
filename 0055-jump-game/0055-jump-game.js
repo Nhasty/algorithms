48909849d3b7161650f8da5787f293ab4390e755
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-02-16 12:30:22 -0700

    Time: 1016 ms (16.45%), Space: 55.6 MB (5.19%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -4,16 +4,16 @@
  */
 var canJump = function(nums) {
   let stack = [0];
-  const visited = new Set();
+  const visited = new Set([0]);
   while (stack.length) {
     const index = stack.pop();
-    visited.add(index);
     if (index >= nums.length - 1) {
       return true;
     }
     for (let i = 1; i <= nums[index]; i += 1) {
       if (!(visited.has(index + i))) {
-        stack.push(index + i);  
+        stack.push(index + i);
+        visited.add(index + i);
       }
     }
   }
