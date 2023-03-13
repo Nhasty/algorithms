1561e229f4af495c1404c80f91106b44c1278385
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-03-13 12:33:34 -0600

    Time: 73 ms (91.03%), Space: 44.2 MB (53.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+  if (s.length !== t.length) {
+    return false;
+  }
+  const tMap = {}; 
+  for (let letter of t) {
+    if(letter in tMap) {
+      tMap[letter] += 1;
+    } else {
+      tMap[letter] = 1;
+    }
+  }
+  for (let letter of s) {
+    if (!(letter in tMap)) {
+      return false
+    }
+    tMap[letter] -= 1;
+    if (tMap[letter] < 0) {
+      return false
+    }
+  }
+  return Object.values(tMap).reduce((a, b) => a + b, 0) === 0;
 };
