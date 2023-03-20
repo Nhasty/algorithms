48f72eb98b3a2934f1c22f5b3b9b90e3348cbfbd
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-03-20 11:26:21 -0600

    Time: 88 ms (55.21%), Space: 41.7 MB (98.11%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,32 +4,19 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-  let left = 0; 
-  let right = 1;
-  let max = 1;
-  let replacements = k;
+  let max = 0;
+  let count = Array(26).fill(0);
+  let left = 0;
+  let right = 0;
   while (right < s.length) {
-    if (s[right] === s[left]) {
-      right += 1;
-    } else {
-      if (replacements > 0) {
-        replacements -= 1;
-        right += 1
-      } else {
-        max = Math.max(max, right - left)
-        const current = s[left];
-        while (s[left] === current) {
-          left += 1;
-        }
-        replacements = k;
-        right = left + 1;
-      }
+    count[s.charCodeAt(right) - 'A'.charCodeAt()] += 1;
+    right += 1;
+    const high = Math.max(...count);
+    const visited = count.reduce((a, b) => a + b, 0);
+    if (visited - high > k) {
+      count[s.charCodeAt(left) - 'A'.charCodeAt()] -= 1;
+      left += 1;
     }
   }
-  while (replacements > 0 && left > 0) {
-    left -= 1;
-    replacements -= 1;
-  }
-  max = Math.max(max, right - left)
-  return max
+  return count.reduce((a, b) => a + b, 0);
 };
