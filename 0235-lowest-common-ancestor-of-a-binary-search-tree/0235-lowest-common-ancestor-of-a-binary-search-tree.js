e6add7ab2172cf0944432ce67f235d296b96d43e
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-03-29 15:04:37 -0600

    Time: 84 ms (62.64%), Space: 52.6 MB (8.88%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,11 +13,13 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+  if ((root.val <= p.val && root.val >= q.val) || (root.val >= p.val && root.val <= q.val)) {
+    return root;
+  }
   if (root.val > p.val && root.val > q.val) {
     return lowestCommonAncestor(root.left, p, q);
   }
   if (root.val < p.val && root.val < q.val) {
     return lowestCommonAncestor(root.right, p, q);
   }
-  return root;
 };
