065e4355b6c0ed59b2f89833b1cb5998f41d307a
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-04-27 17:16:39 -0600

    Time: 88 ms (62.25%), Space: 47.4 MB (57.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,14 +6,16 @@ var lengthOfLongestSubstring = function (s) {
     let set = new Set();
     let left = 0;
     let max = 0;
-    if (s.length <= 1) return s.length;
+    if (s.length <= 1) {
+      return s.length
+    };
     for (let i = 0; i < s.length; i++) {
       while (set.has(s[i])) {
           set.delete(s[left])
           left += 1;
       }
-        set.add(s[i]);
-        max = Math.max(max, i - left + 1)
+      set.add(s[i]);
+      max = Math.max(max, i - left + 1)
     }
     return max;
 }
