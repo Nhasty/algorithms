627a2e42aeb401a3a937c5708beb092b5e72ff8a
Nyall Hasty <66572145+Nhasty@users.noreply.github.com>
2023-05-03 16:13:49 -0600

    Time: 74 ms (85.65%), Space: 45 MB (46.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,18 +3,32 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-  const columns = new Set();
-  for (let row of matrix) {
-    if (row.some((item) => item === 0)) {
-      for (let i = 0; i < row.length; i++) {
-        row[i] === 0 ? columns.add(i) : row[i] = 0;  
+  debugger
+  const firstRowHasZeros = matrix[0].some((item) => item === 0);
+  let firstColHasZeros = !matrix[0][0];
+  for (let i = 1; i < matrix.length; i++) {
+    firstColHasZeros = !matrix[i][0] ? true : firstColHasZeros;
+    for (let j = 1; j < matrix[0].length; j ++) {
+      if (!matrix[i][j]) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
       }
     }
   }
-  for (let row of matrix) {
-    for (let [entry] of columns.entries()) {
-      row[entry] = 0;
+  for (let i = 1; i < matrix.length; i++) {
+    if (!matrix[i][0]) {
+      matrix[i].fill(0);
     }
+  }
+  for (let j = 0; j < matrix[0].length; j++) {
+    if ((j === 0 && firstColHasZeros) || (j > 0 && !matrix[0][j])) {
+      for (let i = 0; i < matrix.length; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  if (firstRowHasZeros) {
+    matrix[0].fill(0);
   }
   return matrix;
 };
